feaaf8cc676058f72f9717e913c2c28ca7511afc
Ayomide Onibokun <ayo6706@gmail.com>
2024-04-30 11:47:09 +0100

    Implement todo graphql type

---
+++ src/todos/todo.type.ts
@@ -0,0 +1,16 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+
+@ObjectType('Todo')
+export class TodoType {
+    @Field(type => ID)
+    id: string;
+
+    @Field()
+    title: string;
+
+    @Field()
+    description: string;
+
+    @Field()
+    completed: boolean;
+}
