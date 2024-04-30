b97d4303bbf1ba20eb2977b110a4493ba7e639fb
Ayomide Onibokun <ayo6706@gmail.com>
2024-04-30 11:30:40 +0100

    typeorm and graphql init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/todos'),
-    TodosModule],
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/todo',
+      synchronize: true,
+      useUnifiedTopology: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true
+    }),
+    TodosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
